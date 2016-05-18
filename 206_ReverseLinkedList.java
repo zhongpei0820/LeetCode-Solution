fe817c506c452b7d477bdc514d50c7c1f635e60d
zhongpei0820 <kevinzhongpei@163.com>
2016-05-18 11:05:12 +0800

    Update 206_ReverseLinkedList.java

--- 206_ReverseLinkedList.java
+++ 206_ReverseLinkedList.java
@@ -1,1 +1,27 @@
+//Reverse a singly linked list.
 
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode p = head;
+        if(head == null) return head;
+        if(head.next == null)return head;
+        ListNode q = head.next;
+        head.next = null;
+        while(q.next != null){
+            ListNode r = q.next;
+            q.next = p;
+            p = q;
+            q = r;
+        }
+        q.next = p;
+        return q;
+    }
+}
