1577adb908e4d44a7e423ecb7e8497bd3bc63cd6
zhongpei0820 <kevinzhongpei@163.com>
2016-05-18 17:47:03 +0800

    Update 263_UglyNumber.java

--- 263_UglyNumber.java
+++ 263_UglyNumber.java
@@ -1,1 +1,25 @@
+// Write a program to check whether a given number is an ugly number.
 
+// Ugly numbers are positive numbers whose prime factors only include 2, 3, 5. For example, 6, 8 are ugly while 14 is not ugly since it includes another prime factor 7.
+
+// Note that 1 is typically treated as an ugly number.
+
+//Idea: divided by 2, 3, 5 iteratively
+
+public class Solution {
+    public boolean isUgly(int num) {
+        if(num == 0) return false;
+        num = dividedBy(num,2);
+        num = dividedBy(num,3);
+        num = dividedBy(num,5);
+        if(num == 1) return true;
+        else return false;
+    }
+    
+    public int dividedBy(int a, int b){
+        while(a % b == 0){
+            a /= b;
+        }
+        return a;
+    }
+}
