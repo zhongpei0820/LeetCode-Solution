6cf20fd6ff16ce975b15a39515ac52f8bd07069a
zhongpei0820 <kevinzhongpei@163.com>
2016-05-18 17:56:48 +0800

    Update 231_PowerOfTwo.java

--- 231_PowerOfTwo.java
+++ 231_PowerOfTwo.java
@@ -1,1 +1,12 @@
 
+// Given an integer, write a function to determine if it is a power of two.
+
+//Idea: An integer n is power of two iff n & (n-1) == 0. 
+//For example: 16 -> 10000, 16 - 1 = 01111, 10000 & 01111 = 00000.  
+
+public class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n < 1) return false;
+        return (0 == (n & (n-1)));
+    }
+}
