6d45f173ac0d61d9724ea475762f29ec400fd2a6
zhongpei0820 <kevinzhongpei@163.com>
2016-05-20 22:02:12 +0800

    Update 9_PalindromeNumber.java

--- 9_PalindromeNumber.java
+++ 9_PalindromeNumber.java
@@ -1,1 +1,16 @@
+// Determine whether an integer is a palindrome. Do this without extra space.
 
+// Idea: reverse the integer and compare it with the orginal one. 
+
+public class Solution {
+    public boolean isPalindrome(int x) {
+        if(x < 0) return false;
+        int num = x;
+        int mir = 0;
+        while(num > 0){
+            mir = 10* mir + num % 10;
+            num /= 10;
+        }
+    return mir == x;
+    }
+}
