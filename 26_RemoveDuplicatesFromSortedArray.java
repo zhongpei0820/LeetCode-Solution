d94c7ee9cac02b0cec4ed7799f64bce946bf0c3b
zhongpei0820 <kevinzhongpei@163.com>
2016-05-20 18:50:51 +0800

    Update 26_RemoveDuplicatesFromSortedArray.java

--- 26_RemoveDuplicatesFromSortedArray.java
+++ 26_RemoveDuplicatesFromSortedArray.java
@@ -1,9 +1,26 @@
-Given a sorted array, remove the duplicates in place such that each element appear only once and return the new length.
+// Given a sorted array, remove the duplicates in place such that each element appear only once and return the new length.
 
-Do not allocate extra space for another array, you must do this in place with constant memory.
+// Do not allocate extra space for another array, you must do this in place with constant memory.
 
-For example,
-Given input array nums = [1,1,2],
+// For example,
+// Given input array nums = [1,1,2],
 
-Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively. 
-It doesn't matter what you leave beyond the new length.
+// Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively. 
+// It doesn't matter what you leave beyond the new length.
+
+//Idea: Because we do not care what we leave beyond the new length and the array is sorted. 
+//      So, when nums[i] != nums[i+1], so we simple replace nums[i+1] with the next larger nums[j] we find.
+
+public class Solution {
+    public int removeDuplicates(int[] nums) {
+        if(nums.length < 2) return nums.length;
+        int sum = 1;
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] != nums[i-1]){
+                nums[sum++] = nums[i];
+            }
+        }
+        
+        return sum;
+    }
+}
