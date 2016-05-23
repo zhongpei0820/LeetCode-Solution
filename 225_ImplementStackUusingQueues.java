cb7bba8afcb482f578bfb87dd397dd47fde1564a
zhongpei0820 <kevinzhongpei@163.com>
2016-05-23 21:57:03 +0800

    Update 225_ImplementStackUusingQueues.java

--- 225_ImplementStackUusingQueues.java
+++ 225_ImplementStackUusingQueues.java
@@ -1,1 +1,40 @@
+// Implement the following operations of a stack using queues.
 
+// push(x) -- Push element x onto stack.
+// pop() -- Removes the element on top of the stack.
+// top() -- Get the top element.
+// empty() -- Return whether the stack is empty.
+
+class MyStack {
+    // Push element x onto stack.
+    Queue<Integer> q = new LinkedList<>();
+    public void push(int x) {
+        if(q.isEmpty()){
+            q.add(x);
+        }else{
+            int[] temp = new int[q.size()];
+            for(int i = 0; i < temp.length; i++){
+                temp[i] = q.poll();
+            }
+            q.add(x);
+            for(int i = 0; i < temp.length; i++){
+                q.add(temp[i]);
+            }
+        }
+    }
+
+    // Removes the element on top of the stack.
+    public void pop() {
+        q.poll();
+    }
+
+    // Get the top element.
+    public int top() {
+        return q.peek();
+    }
+
+    // Return whether the stack is empty.
+    public boolean empty() {
+        return q.isEmpty();
+    }
+}
