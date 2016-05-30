8686acab501ce3b2ab3279aa48878e7f097fcf8a
zhongpei0820 <kevinzhongpei@163.com>
2016-05-30 21:59:43 +0800

    Update 122_BestTimeToBuyAndSellStockII.java

--- 122_BestTimeToBuyAndSellStockII.java
+++ 122_BestTimeToBuyAndSellStockII.java
@@ -1,4 +1,19 @@
-Say you have an array for which the ith element is the price of a given stock on day i.
-Design an algorithm to find the maximum profit.
-You may complete as many transactions as you like (ie, buy one and sell one share of the stock multiple times). 
-However, you may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+// Say you have an array for which the ith element is the price of a given stock on day i.
+// Design an algorithm to find the maximum profit.
+// You may complete as many transactions as you like (ie, buy one and sell one share of the stock multiple times). 
+// However, you may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+
+//Idea: Greed on daily profit. Define daily profit = prices[i] - prices[i - 1].
+//      If daily profit > 0, profit += daily profit.
+
+public class Solution {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        for(int i = 1; i < prices.length; i++){
+            if(prices[i] > prices[i - 1]){
+                profit += (prices[i] - prices[i - 1]);
+            }
+        }
+        return profit;
+    }
+}
