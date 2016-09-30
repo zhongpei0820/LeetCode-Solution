50d510e8366757ea0afd2c23d4ae793167d8e376
Pei Zhong <kevinzhongpei@163.com>
2016-09-30 00:07:30 -0400

    Update 405_ConvertaNumbertoHexadecimal.java

--- 405_ConvertaNumbertoHexadecimal.java
+++ 405_ConvertaNumbertoHexadecimal.java
@@ -1,3 +1,14 @@
+// Given an integer, write an algorithm to convert it to hexadecimal. For negative integer, two’s complement method is used.
+
+// Note:
+
+// All letters in hexadecimal (a-f) must be in lowercase.
+// The hexadecimal string must not contain extra leading 0s. If the number is zero, it is represented by a single zero character '0'; otherwise, the first character in the hexadecimal string will not be the zero character.
+// The given number is guaranteed to fit within the range of a 32-bit signed integer.
+// You must not use any method provided by the library which converts/formats the number to hex directly.
+
+//-1 = "ffffffff". Use '>>>' instead of '>>'.
+
 public class Solution {
     
     String[] hex = {"0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"};
