f8f35a5746fae94471e8e205cb3569244b550d90
zhongpei0820 <kevinzhongpei@163.com>
2016-10-01 20:25:22 -0400

    Organize

---
+++ 100-199/190_ReverseBits.java
@@ -0,0 +1,30 @@
+// Reverse bits of a given 32 bits unsigned integer.
+
+// For example, given input 43261596 (represented in binary as 00000010100101000001111010011100), return 964176192 (represented in binary as 00111001011110000010100101000000).
+
+// Follow up:
+// If this function is called many times, how would you optimize it?
+
+// Related problem: Reverse Integer
+
+// Credits:
+// Special thanks to @ts for adding this problem and creating all test cases.
+
+
+//Solution: 
+//1. Get the last bit of the input.
+//2. Add it to the result.
+//3. Right shift the input and left shift the result.
+
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        int result = 0;
+        for(int i = 0; i < 32; i++){
+            result <<= 1;
+            if((n & 1) == 1) result++;
+            n >>= 1;
+        }
+        return result;
+    }
+}
