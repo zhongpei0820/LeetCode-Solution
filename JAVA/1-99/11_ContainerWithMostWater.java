3b557124879d1efe13ceb6e62ee817af309e8370
zhongpei0820 <kevinzhongpei@163.com>
2016-11-03 20:06:39 -0400

    11/03/2016

---
+++ JAVA/1-99/11_ContainerWithMostWater.java
@@ -0,0 +1,31 @@
+// Given n non-negative integers a1, a2, ..., an, where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). Find two lines, which together with x-axis forms a container, such that the container contains the most water.
+
+// Note: You may not slant the container.
+
+// Solution:
+// Intuition: Solve this with two pointers, left and right.
+// while left < right : 
+//        maxArea = max(maxArea, (right - left) * Math.min(height[left],height[right]))
+//        if height[left] < height[right]: left++
+//        else: right++
+
+// Proof:
+// if we have a1,a2,a3 ... a20, and the max area is between a10 and a15.
+// If the left pointer is pointing at a10, how can we be sure the right pointer can be moved to a15 while the left is pointint at a10.
+// Say now the right is pointing at a16. Then we compare a15 and a11, if a10 < a16, we will move the left to a11.
+// However, this is impossible, because if a10 < a16, then the area between a10 and a15 must be smaller than a10 and a16, which is 
+// against that a10 and a15 is the biggest area.
+// So a10 must be greater than a16, and the next move will be a16 to a15.
+// Therefore, we can find the maximum area.
+
+public class Solution {
+    public int maxArea(int[] height) {
+        int left = 0,right = height.length - 1,max = 0;
+        while(left < right){
+            max = Math.max(max,(right - left) * Math.min(height[left],height[right]));
+            if(height[left] < height[right]) left++;
+            else right--;
+        }
+        return max;
+    }
+}
